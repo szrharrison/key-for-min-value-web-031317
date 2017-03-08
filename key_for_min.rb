eda99ea45f9ba10d5662752aa4bd91a1189a4526
Scott Harrison <s.z.r.harrison@gmail.com>
2017-03-08 22:54:26 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,16 @@
 
 def key_for_min_value(name_hash)
 
+  prev = ''
+  if !name_hash.empty?
+    min = name_hash.first.last
+  end
+  name_hash.each do |key, value|
+    if value < min
+      min = value
+    end
+    prev = value
+  end
+  
+  name_hash.key( min )
 end
